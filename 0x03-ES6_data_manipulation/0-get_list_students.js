d32d80a54c1b0415a6989b310b5ce2be717ff69c
OGOR-STEPHEN <ogorstephen485@gmail.com>
2024-10-10 11:39:54 +0000

    Second commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,12 @@ function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }, // Trailing comma
   ];
 }
+
+// Option 1: Call the function
+console.log(getListStudents());
+
+// Option 2: Export the function
+// module.exports = getListStudents;
